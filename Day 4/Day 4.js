ad4acb825fa57228fb86161b6d6e2ec28cd42e7d
unknown <barokah.ilmi@gmail.com>
2023-12-16 20:36:25 +0700

    main

---
+++ Day 4/Day 4.js
@@ -0,0 +1,20 @@
+/*Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]*/
+
+function capitals(word) {
+    const indexes = [];
+    
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === word[i].toUpperCase()) {
+        indexes.push(i);
+      }
+    }
+    
+    return indexes;
+  }
+  
+  const result = capitals("CodEWaRs");
+  console.log(result);
